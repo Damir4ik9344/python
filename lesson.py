6977706edbc127883ff5cfee1603f3545c86fad4
unknown <khazimov.d@academy.com.ua>
2025-01-27 15:05:16 +0200

    Niger

---
+++ lesson.py
@@ -0,0 +1,17 @@
+n = int(input("Введіть число: "))
+
+for i in range(2, n + 1):
+    if n % 1 == 0:
+        print('First divisor', 1)
+        break 
+
+
+suma = 0
+
+for i in range (10):
+    num = int(input())
+    if num > 15 or mun < :
+        break 
+    suma +- num
+
+print(suma)

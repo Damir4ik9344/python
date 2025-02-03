b8e2af4d8ee5e021f79073df725d53677e695340
unknown <khazimov.d@academy.com.ua>
2025-02-03 15:08:50 +0200

    Update

--- today.py
+++ today.py
@@ -1,7 +1,7 @@
 def check_password(password):
     correct_password = "Academy24"
-    if password == correct_password
-    return True
+    if password == correct_password:
+        return True
     return False
 
 
@@ -10,11 +10,25 @@ def brute_force(password_list):
     print("Checking password:{password}")
     if check_password(password):
         print("ACCESS GRANTED!Password is {password}")
-         
-        return password
-              print("ACCESS DANTED !Password is {password}")
-              return None
+         return password
+print("ACCESS DANTED !Password is {password}")
+return None
 
+
+passwords = [
+hX7#uF9&bLp
+T9jH*2q$VwT1
+mNr8$zP1tQx
+4kTpR9!wXsE
+cV6jK0@dZlM
+gWf$7Hp3mYq
+vJ7#e8zZxFg
+WnU2$R1hFbZ
+p3GvQy$L9Js
+F!sR0t8VbWz]
+
+password = [password.strip() for password in passwords]
+brute_force(passwords)
 
 
 

6a744a97f23f635cba797e3830b1ad5d8c1a7b84
unknown <khazimov.d@academy.com.ua>
2025-02-10 15:05:42 +0200

    Update

--- today.py
+++ today.py
@@ -1,33 +1,29 @@
 def check_password(password):
-    correct_password = "Academy24"
+    correct_password = "gWf$7Hp3mYq"
     if password == correct_password:
         return True
     return False
 
-
 def brute_force(password_list):
- for password in password_list:
-    print("Checking password:{password}")
-    if check_password(password):
-        print("ACCESS GRANTED!Password is {password}")
-         return password
-print("ACCESS DANTED !Password is {password}")
-return None
+    for password in password_list:
+        print(f"Checking password:{password}")
+        if check_password(password):
+            print(f"ACCESS GRANTED! {password}")
+            return password
+    print("ACCESS DANTED!")
+    return None
 
 
-passwords = [
-hX7#uF9&bLp
-T9jH*2q$VwT1
-mNr8$zP1tQx
-4kTpR9!wXsE
-cV6jK0@dZlM
-gWf$7Hp3mYq
-vJ7#e8zZxFg
-WnU2$R1hFbZ
-p3GvQy$L9Js
-F!sR0t8VbWz]
+passwords = ["hX7#uF9&bLp", 
+"T9jH*2q$VwT1",
+"kTpR9!wXsE",
+"cV6jK0@dZlM",
+"gWf$7Hp3mYq",
+"vJ7#e8zZxFg",
+"WnU2$R1hFbZ",
+"p3GvQy$L9Js",
+"F!sR0t8VbWz"]
 
-password = [password.strip() for password in passwords]
 brute_force(passwords)
 
 
